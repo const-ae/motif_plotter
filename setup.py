d261cc49ac7f5af43341bb0132048f89121022e6
Constantin <const-ae@users.noreply.github.com>
2019-04-11 18:01:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name='motif_plotter',
       version='0.2',
       description='A simple tool to plot sequence motifs with variable importance scores',
       author='Constantin Ahlmann-Eltze',
-      author_email='constantin.ahlmann@gmail.com'
+      author_email='artjom31415@googlemail.com'
      )
